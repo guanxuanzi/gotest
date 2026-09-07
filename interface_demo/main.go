080bf213fb189ba79941c2fcbb7c05d8d70c0de3
guojiabao <yudao789456123@163.com>
2026-09-07 16:22:47 +0800

    feat(interface):新增类型断言练习

--- interface_demo/main.go
+++ interface_demo/main.go
@@ -26,10 +26,40 @@ func (c Cat) Speak() string {
 	return "Meow!"
 }
 
+// =========================================
+// 空接口
+func print(a interface{}) {
+	fmt.Println(a)
+}
+
+// 空接口用于验证类型断言
+var x interface{} = "Hello,Go!"
+
 func main() {
 	dog := Dog{Name:"Rex"}
 	cat := Cat{Name:"Whiskers"}
 	// 满足接口要求的结构体都可以传入
 	Speak(dog)
 	Speak(cat)
+
+	print("=========================================")
+// =========================================
+// 空接口类型的切片:可以放入任意类型
+	a := []interface{}{"nihao",2,true}
+	print(a)
+	// key为string,valuer味空接口的Map
+	b := map[string]interface{}{"name":"张三","age":20,"gender":"男"}
+	print(b)
+
+	// 结构体字段使用空接口
+	c := struct {Name interface{}}{Name:"张三"}
+	print(c)
+
+
+	// 用于练习类型断言x.(string)
+	if value, ok := x.(string);ok {
+		fmt.Println("x是string:",value)
+	} else {
+		fmt.Println("x不是string")
+	}
 }
